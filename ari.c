29b68174203799244a2038507da20add69498640
srk830 <84616986+srk830@users.noreply.github.com>
2021-06-30 09:16:41 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -8,20 +8,20 @@ string ari(string s) {
     float b = (float)nw/ns;
     int x = 4.71 * a + 0.5 * b - 21.43;
     switch(x+1) {
-      case 1:   return Kindergarten;
-      case 2:   return First/Second Grade;
-      case 3:   return Third Grade;
-      case 4:   return Fourth Grade;
-      case 5:   return Fifth Grade;
-      case 6:   return Sixth Grade;
-      case 7:   return Seventh Grade;
-      case 8:   return Eighth Grade;
-      case 9:   return Ninth Grade;
-      case 10:  return Tenth Grade;
-      case 11:  return Eleventh Grade;
-      case 12:  return Twelfth grade;
-      case 13:  return College student;
-      case 14:  return Professor;
+      case 1:   return "Kindergarten;
+      case 2:   return "First/Second Grade";
+      case 3:   return "Third Grade";
+      case 4:   return "Fourth Grade";
+      case 5:   return "Fifth Grade";
+      case 6:   return "Sixth Grade";
+      case 7:   return "Seventh Grade";
+      case 8:   return "Eighth Grade";
+      case 9:   return "Ninth Grade";
+      case 10:  return "Tenth Grade";
+      case 11:  return "Eleventh Grade";
+      case 12:  return "Twelfth grade";
+      case 13:  return "College student";
+      case 14:  return "Professor";
     }
   }
 }
